10d66853f0a1c3812d660a35af062d3946045c57
29supriya <110988171+29supriya@users.noreply.github.com>
2022-09-13 10:12:19 +0530

    initial commit

---
+++ singleentity/src/main/java/com/employee/singleentity/SingleentityApplication.java
@@ -0,0 +1,13 @@
+package com.employee.singleentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleentityApplication.class, args);
+	}
+
+}
---
+++ singleentity/src/test/java/com/employee/singleentity/SingleentityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.singleentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleentityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
